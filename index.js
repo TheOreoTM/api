59cdb1726a06c97ce0d19d4d64163df776d92fea
OreoTM <qwertzybad@gmail.com>
2022-11-20 11:38:31 +0500

    factorise

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -31,65 +32,20 @@ app.get('/sub', (req, res) => {
   res.send({ output: total });
 });
 
-app.get('/story', (req, res) => {
-  const story = req.query.text;
-  const words = countWords(story);
-
-  res.write(`
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Current Story</title>
-    <link
-      rel="shortcut icon"
-      href="https://cdn.discordapp.com/attachments/907414829445283870/1043405131422629939/skittletrans.png"
-      type="image/x-icon"
-    />
-    <meta content="Cuurent Story" property="og:title" />
-    <meta
-      content="View the current progress of the story"
-      property="og:description"
-    />
-    <meta content="https://discord.gg/vzdGeVrxZk" property="og:url" />
-    <meta content="" property="og:image" />
-    <meta content="#ffffff" data-react-helmet="true" name="theme-color" />
-  </head>
-
-  <body bgcolor="#121212">
-    <div id="box">
-      <center>
-        <h1 style="color: #ffffff; font-family: 'Montserrat', sans-serif">
-          Current Story - ${words} Words
-        </h1>
-      </center>
-      <hr
-        style="
-          height: 2px;
-          width: 30%;
-          border-width: 0;
-          color: gray;
-          background-color: gray;
-        "
-      />
-      <p
-        style="
-          padding: 10px 70px;
-          font-size: 1.2rem;
-          color: #ffffff;
-          font-family: 'Montserrat', sans-serif;
-        "
-      >
-        ${story}
-      </p>
-    </div>
-
-    <script src="index.js"></script>
-  </body>
-</html>
-`);
-  res.send();
+app.get('/factor', (req, res) => {
+  const equation = req.query.equation;
+  fetch(`https://newton.vercel.app/api/v2/factor/${equation}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error('ERROR');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      res.send({
+        data,
+      });
+    });
 });
 
 app.listen(PORT, () => console.log(`Alive on http://localhost:${PORT}`));
