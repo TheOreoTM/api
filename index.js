3cf7ff16974c72b392bfc9a40e2b215da70640ef
OreoTM <qwertzybad@gmail.com>
2022-12-01 08:52:54 +0500

    connecting with ESPN to provide more data

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ const endpoints = ['/story', '/add', `/sub`, `/factorise`, `/checkWord`];
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-async function getData(link) {
-  fetch(link)
-    .then((response) => {
-      if (!response.ok) {
-        throw Error('ERROR');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data;
-    });
+function convertDateToEpoch(date) {
+  return (new Date(date).valueOf() / 1000).toFixed(0);
 }
+
+const getJSON = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok)
+    // check if response worked (no 404 errors etc...)
+    throw new Error(response.statusText);
+
+  const data = response.json(); // get JSON from the response
+  return data; // returns a promise, which resolves to this data value
+};
+
 function countWords(str) {
   const arr = str.split(' ');
 
@@ -83,7 +85,7 @@ app.get('/factorise', (req, res) => {
     });
 });
 
-app.get(`/worldCup/next`, (req, res) => {
+app.get(`/worldcup/next`, (req, res) => {
   fetch('https://worldcupjson.net/matches/today/?by_date=ASC')
     .then((response) => {
       if (!response.ok) {
@@ -92,8 +94,9 @@ app.get(`/worldCup/next`, (req, res) => {
       return response.json();
     })
     .then((data) => {
+      // Getting by id
       let output = [];
-      data.forEach((element, index) => {
+      data.forEach((element) => {
         data = element;
 
         output.push({
@@ -109,4 +112,79 @@ app.get(`/worldCup/next`, (req, res) => {
     });
 });
 
+app.get('/worldcup/find/:id', (req, res) => {
+  const id = req.params.id;
+  let matchData;
+  fetch(`https://worldcupjson.net/matches?details=true`)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error('ERROR');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // worldcup.json
+      const matchIndex = data.findIndex((match) => match.id == id);
+      matchData = data[matchIndex];
+      let matchTime = convertDateToEpoch(data[matchIndex].datetime);
+
+      //espn
+      let ESPNData;
+      getJSON(
+        'https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/scoreboard?limit=950&dates=20221001-20230130'
+      )
+        .then((data) => {
+          // Stuff goes below here
+          let ESPNIndex = data.events.findIndex(
+            (match) => convertDateToEpoch(match.date) === matchTime
+          );
+          // Delete useless data
+          ESPNData = data.events[ESPNIndex];
+          delete ESPNData.links;
+          delete ESPNData.season;
+          delete ESPNData.uid;
+          delete ESPNData.date;
+          ESPNData.competitors = ESPNData.competitions[0].competitors;
+          delete ESPNData.competitions;
+          delete matchData.home_team_statistics;
+          delete matchData.away_team_statistics;
+          matchData.home_team_statistics = ESPNData.competitors[0].statistics;
+          matchData.away_team_statistics = ESPNData.competitors[1].statistics;
+          matchData.home_team.logo = ESPNData.competitors[0].team.logo;
+          matchData.away_team.logo = ESPNData.competitors[1].team.logo;
+          matchData.home_team.color = [
+            ESPNData.competitors[0].team.color,
+            ESPNData.competitors[0].team.alternateColor,
+          ];
+          matchData.away_team.color = [
+            ESPNData.competitors[1].team.color,
+            ESPNData.competitors[1].team.alternateColor,
+          ];
+          matchData.name = ESPNData.name;
+          matchData.espnId = ESPNData.id;
+          delete matchData.officials;
+          delete matchData.detailedTime;
+          matchData.detailedTime = ESPNData.status;
+          matchData.timestamp = convertDateToEpoch(matchData.datetime);
+
+          res.send(matchData);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+});
 app.listen(PORT, () => console.log(`Alive on http://localhost:${PORT}`));
+
+// app.get('/test', (req, res) => {
+//   console.log('Fetching data...');
+//   getJSON(
+//     'https://soundcloud.com/oembed?url=http%3A//soundcloud.com/forss/flickermood&format=json'
+//   )
+//     .then((data) => {
+//       // Stuff goes below here
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// });
